dc3b190c66b0e27fc786a00a368c64176a4e39cf
Navya-git-account <bommishettinavya@gmail.com>
2026-09-13 15:46:00 -0500

    Add student registration and login backend authentication

---
+++ backend/src/main/java/com/studenttracker/student_tracker/repository/StudentRepository.java
@@ -0,0 +1,14 @@
+package com.studenttracker.student_tracker.repository;
+
+import com.studenttracker.student_tracker.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository
+        extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
